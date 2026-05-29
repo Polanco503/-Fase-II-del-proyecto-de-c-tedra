926fd6f5bd8030dcdb4e3ba141abe9d95c609995
Polanco503 <polancoamaya12@gmail.com>
2026-05-29 16:46:43 -0600

    Remove legacy duplicate package files

--- src/main/java/sv/edu/udb/restaurant/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package sv.edu.udb.restaurant.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sv.edu.udb.restaurant.model.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- src/main/java/sv/edu/udb/restaurant/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package sv.edu.udb.restaurant.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sv.edu.udb.restaurant.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
